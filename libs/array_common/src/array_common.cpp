71b9a90e0326b7fdb14a491fd312b4ea3a7270de
el smit <79165299+elasmit@users.noreply.github.com>
2021-04-06 22:16:46 +0100

    Dev (#9)
    * 04lb-1
    
    * Lab 4 5 (#2)
    
    * lab04
    
    * lab-05
    
    * lab-05-1
    
    * final touch lab 5
    
    * Don't code during nights
    
    * bits
    
    * readme
    
    * readme in root
    
    * lol
    
    * File reorg
    
    * l07
    
    * minor fix
    
    * make update
    
    * make update
    
    * Google unit tests added

--- libs/array_common/src/array_common.cpp
+++ libs/array_common/src/array_common.cpp
@@ -37,3 +37,42 @@ void arrays::print(const std::vector<std::pair<double, double>>& in)
         std::cout << ceil(in[i].second*100)/100 << "\t";
     std::cout << std::endl;
 }
+
+template <class T>
+    T arrays::sum(const std::vector<T>& in)
+    {
+        T sum = 0;
+        for (int i = 0; i < in.size(); i++)
+            sum += in[i];
+
+        return sum;
+    }
+
+    int arrays::sum(const std::vector<int>& in)
+    {
+        int sum = 0;
+        for (int i = 0; i < in.size(); i++)
+            sum += in[i];
+
+        return sum;
+    }
+
+    double arrays::sum(const std::vector<double>& in)
+    {
+        double sum = 0;
+        for (int i = 0; i < in.size(); i++)
+            sum += in[i];
+
+        return sum;
+    }
+
+    float arrays::sum(const std::vector<float>& in)
+    {
+        float sum = 0;
+        for (float i = 0; i < in.size(); i++)
+            sum += in[i];
+
+        return sum;
+    }
+
+    
