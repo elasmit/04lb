58c7676b3e9b40f310dfece2c69bfda6c079fe38
Elasmit <elasmitgame@gmail.com>
2021-05-01 21:59:23 +0100

    l08

--- libs/array_common/src/array_common.cpp
+++ libs/array_common/src/array_common.cpp
@@ -1,32 +1,6 @@
 #include "array_common.hpp"
 #include <cmath>
 
-template<class T>
-void arrays::print(const T& in, int size)
-    {
-        for (int i = 0; i < size; i++)
-            std::cout << in[i] << std::endl;
-    }
-
-template<class T>
-    void arrays::print(const std::vector<T>& in)
-    {
-        for (int i = 0; i < in.size(); i++)
-            std::cout << in[i] << std::endl;
-    }
-
-template<class T1, class T2>
-    void arrays::print(const std::vector<std::pair<T1, T2>>& in)
-    {
-        for (int i = 0; i < in.size(); i++)
-            std::cout << ceil(in[i].first*100)/100 << "\t";
-        std::cout << std::endl;
-
-        for (int i = 0; i < in.size(); i++)
-            std::cout << ceil(in[i].second*100)/100 << "\t";
-        std::cout << std::endl;
-    }
-
 void arrays::print(const std::vector<std::pair<double, double>>& in)
 {
     for (int i = 0; i < in.size(); i++)
@@ -37,16 +11,6 @@ void arrays::print(const std::vector<std::pair<double, double>>& in)
         std::cout << ceil(in[i].second*100)/100 << "\t";
     std::cout << std::endl;
 }
-
-template <class T>
-    T arrays::sum(const std::vector<T>& in)
-    {
-        T sum = 0;
-        for (int i = 0; i < in.size(); i++)
-            sum += in[i];
-
-        return sum;
-    }
 
     int arrays::sum(const std::vector<int>& in)
     {
