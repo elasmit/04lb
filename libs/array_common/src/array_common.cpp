36225d65fb87d8c79c671dabe29d12507b6e8015
el smit <79165299+elasmit@users.noreply.github.com>
2021-05-01 22:16:55 +0100

    Dev (#11)
    * 04lb-1
    
    * Lab 4 5 (#2)
    
    * lab04
    
    * lab-05
    
    * lab-05-1
    
    * final touch lab 5
    
    * Don't code during nights
    
    * bits
    
    * readme
    
    * readme in root
    
    * lol
    
    * File reorg
    
    * l07
    
    * minor fix
    
    * make update
    
    * make update
    
    * Google unit tests added
    
    * Google unit tests desc
    
    * ss
    
    * ss2
    
    * md l08
    
    Co-authored-by: kokivanov <kokivanov@gmail.com>
    Co-authored-by: elasmit <kokiiivanov@gmail.com>
    Co-authored-by: elasmit <elasmitgame@gmail.com>

--- libs/array_common/src/array_common.cpp
+++ libs/array_common/src/array_common.cpp
@@ -36,6 +36,36 @@ void arrays::print(const std::vector<std::pair<double, double>>& in)
         for (float i = 0; i < in.size(); i++)
             sum += in[i];
 
+
+    for (int i = 0; i < in.size(); i++)
+        std::cout << ceil(in[i].second*100)/100 << "\t";
+    std::cout << std::endl;
+}
+
+    int arrays::sum(const std::vector<int>& in)
+    {
+        int sum = 0;
+        for (int i = 0; i < in.size(); i++)
+            sum += in[i];
+
+        return sum;
+    }
+
+    double arrays::sum(const std::vector<double>& in)
+    {
+        double sum = 0;
+        for (int i = 0; i < in.size(); i++)
+            sum += in[i];
+
+        return sum;
+    }
+
+    float arrays::sum(const std::vector<float>& in)
+    {
+        float sum = 0;
+        for (float i = 0; i < in.size(); i++)
+            sum += in[i];
+      
         return sum;
     }
 
